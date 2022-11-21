f915983b54e1b0fb188afe126bc449d237f7e9aa
Madeline Ariana Pérez Quiñónez <3062932420313@ingenieria.usac.edu.gt>
2022-11-21 13:14:24 -0600

    customhooks

--- 05-hook-app/src/main.jsx
+++ 05-hook-app/src/main.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { FormWithCustomHook } from './02-useEffect/FormWithCustomHook'
 //import { CounteCustomHook } from './01-useState/CounteCustomHook'
-import { SimpleForm } from './02-useEffect/SimpleForm'
+//import { SimpleForm } from './02-useEffect/SimpleForm'
 //import { CounterApp } from './01-useState/CounterApp'
 //import {HooksApp} from './HooksApp'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   //<React.StrictMode>
-    <SimpleForm />
+    <FormWithCustomHook />
   //</React.StrictMode>
 )
