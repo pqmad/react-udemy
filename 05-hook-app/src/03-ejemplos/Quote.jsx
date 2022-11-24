bdb0a33e462c14b6712ab184d0ef9570ea65b89c
Madeline Ariana Pérez Quiñónez <3062932420313@ingenieria.usac.edu.gt>
2022-11-24 15:22:43 -0600

    hook Layout

--- 05-hook-app/src/03-ejemplos/Quote.jsx
+++ 05-hook-app/src/03-ejemplos/Quote.jsx
@@ -1,9 +1,29 @@
+import { useRef, useState } from "react";
+import { useLayoutEffect } from "react";
 
-export const Quote = ({quote,author}) => {
+export const Quote = ({ quote, author }) => {
+    const pRef = useRef()
+    const [boxSize, setBoxSize] = useState({ width: 0, height: 0 })
+    useLayoutEffect(() => {
+        const{width,height}=(pRef.current.getBoundingClientRect())
+        setBoxSize({width, height})
+    }, [quote])
+
+    
     return (
-        <blockquote className="blockquote text-end">
-            <p className="mb-1">{quote}</p>
-            <footer className="blockquote-footer">{author}</footer>
-        </blockquote>
+        <>
+            <blockquote className="blockquote text-end" style={{ display: 'flex' }}>
+                <p ref={pRef} className="mb-1">{quote}</p>
+                <footer className="blockquote-footer">{author}</footer>
+            </blockquote>
+            <code>{JSON.stringify(boxSize)}</code>
+            <hr/>
+        </>
     )
 }
+
+
+/*
+useLayoutEffect --->
+Use esto para leer el diseño del DOM y volver a renderizar de forma sincrónica.
+*/
--- 05-hook-app/src/main.jsx
+++ 05-hook-app/src/main.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 //import { FormWithCustomHook } from './02-useEffect/FormWithCustomHook'
-import { MultipleCustomHooks } from './03-ejemplos/MultipleCustomHooks'
+//import { MultipleCustomHooks } from './03-ejemplos/MultipleCustomHooks'
+import { FocusScreen } from './04-useRef/FocusScreen'
+import { Layout } from './05-useLayoutEffect/Layout'
 //import { CounteCustomHook } from './01-useState/CounteCustomHook'
 //import { SimpleForm } from './02-useEffect/SimpleForm'
 //import { CounterApp } from './01-useState/CounterApp'
@@ -10,6 +12,6 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   //<React.StrictMode>
-    <MultipleCustomHooks />
+    <Layout />
   //</React.StrictMode>
 )
